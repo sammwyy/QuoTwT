5724e75900b133a5a7a0d332f562a0abc2ca0f75
Sammwy <sammwy.dev@gmail.com>
2021-08-12 19:12:44 -0300

    Added frontend route and example view

--- src/app.js
+++ src/app.js
@@ -1,5 +1,18 @@
 import express from "express";
+import path from "path";
+
+import indexRoutes from "./frontend/routes/index.routes";
 
 const app = express();
+
+/* Settings */
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "frontend", "views"));
+
+/* Middleware */
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+/* Routes */
+app.use("/", indexRoutes);
 
 export default app;
